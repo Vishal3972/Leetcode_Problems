b8b7c27fca7e6ec01b01c163e83e976c001c5770
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-08-06 18:23:52 +0530

    Time: 4 ms (85.24%), Space: 41.8 MB (96.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int ch[] = new int[256];
+        for(int i=0; i<s.length(); i++){
+            int temp = s.charAt(i);
+            ch[temp] = ch[temp]+1;
+        }
+        for(int i=0; i<t.length(); i++){
+            int temp = t.charAt(i);
+            ch[temp] = ch[temp]-1;
+        }
+        for(int i=0; i<ch.length; i++){
+            if(ch[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
