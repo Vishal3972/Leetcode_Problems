fe01536d94a773ef2e152aa3a2493640eeab672e
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-08-15 13:20:24 +0530

    Time: 3 ms (45.11%), Space: 40.6 MB (92.36%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int arr[] = new int[256];
+        for(int i=0;i<s.length();i++){
+            int temp = s.charAt(i);
+            arr[temp]++;
+        }
+        for(int i=0;i<t.length();i++){
+            int temp = t.charAt(i);
+            arr[temp]--;
+        }
+        char ch = '\0';
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                ch = (char)i;
+                break;
+            }
+        }
+       return ch;
+    }
+}
