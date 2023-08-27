bee0b257b22845073249be6b5a638d87f986a2ae
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-08-27 09:33:59 +0530

    Time: 15 ms (61.95%), Space: 43.1 MB (43.97%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,17 +1,42 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         
-        int sum = nums[0] + nums[1] + nums[2];;
-        for(int i=0; i<nums.length-2; i++){
-            for(int j=i+1; j<nums.length-1; j++){
-                for(int k=j+1; k<nums.length; k++){
-                    int temp = nums[i] + nums[j] + nums[k];
-                    if (Math.abs(temp - target) < Math.abs(sum - target)) {
-                        sum = temp;
-                    }
+//         int sum = nums[0] + nums[1] + nums[2];;
+//         for(int i=0; i<nums.length-2; i++){
+//             for(int j=i+1; j<nums.length-1; j++){
+//                 for(int k=j+1; k<nums.length; k++){
+//                     int temp = nums[i] + nums[j] + nums[k];
+//                     if (Math.abs(temp - target) < Math.abs(sum - target)) {
+//                         sum = temp;
+//                     }
+//                 }
+//             }
+//         }
+//         return sum;
+        
+        //  BETTER APPROACH
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+                if (currentSum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-        return sum;
+
+        return closestSum;
     }
 }
