4ddecc674eaf9f52d9fb86ffb2ecbd7eb739172d
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-08-27 09:54:02 +0530

    Time: 0 ms (100.00%), Space: 56.2 MB (20.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,20 +1,36 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if(nums.length <= 1 || nums.length == k){
-            return;
-        }
-        int rotation = k%nums.length;
-        int ans[] = new int[nums.length];
-        int n = 0;
-        for(int i=nums.length-rotation; i<nums.length; i++){
-            ans[n++] = nums[i];
-        }
-        for(int i=0; i<nums.length-rotation; i++){
-            ans[n++] = nums[i];
-        }
-        n = 0;
-        for(int i:ans){
-            nums[n++] = i;
+        // if(nums.length <= 1 || nums.length == k){
+        //     return;
+        // }
+        // int rotation = k%nums.length;
+        // int ans[] = new int[nums.length];
+        // int n = 0;
+        // for(int i=nums.length-rotation; i<nums.length; i++){
+        //     ans[n++] = nums[i];
+        // }
+        // for(int i=0; i<nums.length-rotation; i++){
+        //     ans[n++] = nums[i];
+        // }
+        // n = 0;
+        // for(int i:ans){
+        //     nums[n++] = i;
+        // }
+        
+        // BETTERED SPACE COMPLEXITY
+        k %= nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    
+    public void reverse(int arr[], int s, int e){
+        while(s < e){
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
         }
     }
 }
