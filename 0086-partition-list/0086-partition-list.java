8f4bf9df617eaa7b11e44a2834479971c5c05df2
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-10-02 18:28:42 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (63.22%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,19 +10,14 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head==null || head.next==null){
-            return head;
-        }
- 
         ListNode head1=new ListNode(1);
         ListNode head2=new ListNode(1);
         ListNode temp1=head1;
         ListNode temp2=head2;
-        ListNode cur=head;
         
-        while(cur!=null){
-            ListNode newNode=new ListNode(cur.val);
-            if(cur.val<x){
+        while(head!=null){
+            ListNode newNode=new ListNode(head.val);
+            if(head.val<x){
                 head1.next=newNode;
                 head1=head1.next;
             }
@@ -30,17 +25,9 @@ class Solution {
                 head2.next=newNode;
                 head2=head2.next;
             }
-            cur=cur.next;
+            head=head.next;
         }
-        temp1=temp1.next;
-        temp2=temp2.next;
-        if(temp1==null){
-            return temp2;
-        }
-        if(temp2==null){
-            return temp1;
-        }
-        head1.next=temp2;
-        return temp1;
+        head1.next=temp2.next;
+        return temp1.next;
     }
 }
