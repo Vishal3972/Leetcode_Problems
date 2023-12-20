ae22916c5efee3a017b89630b3be4d868074c101
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-12-20 19:52:49 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (5.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,14 +1,27 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int i=1;
-        while(i<prices.length && prices[i]<=money){
-            int sum=prices[i-1]+prices[i];
-            if(sum<=money){
-                return money-sum;
+        // Arrays.sort(prices);
+        // int i=1;
+        // while(i<prices.length && prices[i]<=money){
+        //     int sum=prices[i-1]+prices[i];
+        //     if(sum<=money){
+        //         return money-sum;
+        //     }
+        //     i++;
+        // }
+        // return money;
+        
+        int small=201;
+        int secondsmall=201;
+        for(int i:prices){
+            if(i<small){
+                secondsmall=small;
+                small=i;
+            }else if(i<secondsmall){
+                secondsmall=i;
             }
-            i++;
         }
-        return money;
+        int sum=small+secondsmall;
+        return (sum<=money)?money-sum:money;
     }
 }
