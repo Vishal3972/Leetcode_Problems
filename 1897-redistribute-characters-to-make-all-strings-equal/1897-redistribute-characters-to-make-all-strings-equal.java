0e29ab8b3d22c96dfa49170692447ec46465b2f6
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-12-30 16:50:59 +0530

    Time: 23 ms (7.92%), Space: 44.1 MB (6.04%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean makeEqual(String[] words) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(String word:words){
+            for(var c:word.toCharArray()){
+                map.put(c,map.getOrDefault(c,0)+1);
+            }
+        }
+        
+        int n=words.length;
+        for(Character c:map.keySet()){
+            int freq=map.get(c);
+            if(freq%n != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
