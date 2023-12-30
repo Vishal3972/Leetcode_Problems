18da52e7305022defe424c732cd7bac6c3031d16
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2023-12-30 13:01:55 +0530

    Time: 212 ms (21.86%), Space: 55.5 MB (25.35%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,28 +1,44 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
-        StringBuffer str = new StringBuffer("");
-        int i=0;
+        // StringBuffer str = new StringBuffer("");
+        // int i=0;
+        // Collections.sort(dictionary);
+        // while(i<sentence.length()){
+        //     int k=i;
+        //     while(i<sentence.length() && sentence.charAt(i)!=' '){
+        //         i++;
+        //     }
+        //     boolean b=false;
+        //     for(String word:dictionary){
+        //         if((sentence.substring(k,i).startsWith(word))){
+        //             str.append(word);
+        //             b=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!b){
+        //         str.append(sentence.substring(k,i));
+        //     }
+        //     str.append(" ");
+        //     i++;
+        // } 
+        // return str.toString().trim();
+        
+        String arr[] = sentence.split(" ");
         Collections.sort(dictionary);
-        while(i<sentence.length()){
-            int k=i;
-            while(i<sentence.length() && sentence.charAt(i)!=' '){
-                i++;
-            }
-            boolean b=false;
-            for(String word:dictionary){
-                if((sentence.substring(k,i).startsWith(word))){
-                    str.append(word);
-                    b=true;
+        for(int i=0;i<arr.length;i++){
+            String word=arr[i];
+            for(String root:dictionary){
+                if(word.startsWith(root)){
+                    arr[i]=root;
                     break;
                 }
             }
-            if(!b){
-                str.append(sentence.substring(k,i));
-            }
-            str.append(" ");
-            i++;
         }
-            
-        return str.toString().trim();
+        String ans=arr[0];
+        for(int i=1;i<arr.length;i++){
+            ans+=" "+arr[i];
+        }
+        return ans;
     }
 }
