17ee14e51c08abc57473655b29b570560a253795
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2024-01-12 10:51:21 +0530

    Time: 8 ms (28.00%), Space: 41.8 MB (28.59%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,14 +3,20 @@ class Solution {
         int n=s.length();
         int count=0;
         s=s.toLowerCase();
-        for(int i=0;i<n/2;i++){
+        // for(int i=0;i<n/2;i++){
+        //     if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+        //         count++;
+        //     }
+        // }
+        // for(int i=n/2;i<n;i++){
+        //     if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+        //         count--;
+        //     }
+        // }
+        for(int i=0;i<n;i++){
             if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
-                count++;
-            }
-        }
-        for(int i=n/2;i<n;i++){
-            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
-                count--;
+                if(i<n/2){count++;}
+                else{count--;}
             }
         }
         return count==0;
