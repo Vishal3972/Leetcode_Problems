21b403041547e7b1d0a16ff707ab5a04f4d06852
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2024-03-17 16:18:26 +0530

    Time: 1 ms (98.87%), Space: 44.7 MB (74.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -5,24 +5,24 @@ class Solution {
         int start = newInterval[0], end = newInterval[1];
         boolean inserted = false;
         
-        for (int[] inv : intervals) {
-            int cstart = inv[0], cend = inv[1];
+        for (int[] arr : intervals) {
+            int a = arr[0], b = arr[1];
             
-            if (cend < start || inserted) {
-                ans.add(new int[]{cstart, cend});
+            if (b < start || inserted) {
+                ans.add(new int[]{a, b});
                 continue;
             }
             
-            start = Math.min(start, cstart);
-            if (end < cstart) {
+            start = Math.min(start, a);
+            if (end < a) {
                 ans.add(new int[]{start, end});
-                ans.add(new int[]{cstart, cend});
+                ans.add(new int[]{a, b});
                 inserted = true;
                 continue;
             }
             
-            if (end <= cend) {
-                ans.add(new int[]{start, cend});
+            if (end <= b) {
+                ans.add(new int[]{start, b});
                 inserted = true;
             }
         }
