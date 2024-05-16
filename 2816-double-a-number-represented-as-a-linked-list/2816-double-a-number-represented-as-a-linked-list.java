34bda7c098bf6a580c83b67a67c24f1c6c475f8c
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2024-05-16 21:38:25 +0530

    Time: 2 ms (99.99%), Space: 47 MB (13.89%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -9,38 +9,60 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head){
-        ListNode prev = null;
-        ListNode cur = head;
-        ListNode nextNode = head;
-        while(nextNode != null){
-            nextNode = nextNode.next;
-            cur.next = prev;
-            prev = cur;
-            cur = nextNode;
-        }
-        return prev;
-    }
+    // Approach 1
     
+//     public ListNode reverseList(ListNode head){
+//         ListNode prev = null;
+//         ListNode cur = head;
+//         ListNode nextNode = head;
+//         while(nextNode != null){
+//             nextNode = nextNode.next;
+//             cur.next = prev;
+//             prev = cur;
+//             cur = nextNode;
+//         }
+//         return prev;
+//     }
+    
+//     public ListNode doubleIt(ListNode head) {
+//         ListNode rev = reverseList(head);
+//         int carry = 0;
+//         ListNode cur = rev, prev = null;
+        
+//         while(cur != null){
+//             int ans = cur.val*2 + carry;
+//             cur.val = ans%10;
+            
+//             carry = ans/10;
+            
+//             prev = cur;
+//             cur = cur.next;
+//         }
+        
+//         if(carry > 0){
+//             prev.next = new ListNode(carry);
+//         }
+        
+//         return reverseList(rev);
+//     }
+    
+    
+    
+    // Appraoach 2
     public ListNode doubleIt(ListNode head) {
-        ListNode rev = reverseList(head);
-        int carry = 0;
-        ListNode cur = rev, prev = null;
-        
-        while(cur != null){
-            int ans = cur.val*2 + carry;
-            cur.val = ans%10;
-            
-            carry = ans/10;
-            
-            prev = cur;
-            cur = cur.next;
+        if(head.val > 4){
+            head = new ListNode(0,head);
         }
         
-        if(carry > 0){
-            prev.next = new ListNode(carry);
+        ListNode tmp = head;
+        while(tmp != null){
+            tmp.val = (tmp.val*2)%10;
+            
+            if(tmp.next != null && tmp.next.val > 4){
+                tmp.val++;
+            }
+            tmp = tmp.next;
         }
-        
-        return reverseList(rev);
+        return head;
     }
 }
