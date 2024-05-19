4865c712460471fb253d42675a8849209a11b8ce
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2024-05-19 11:31:32 +0530

    Time: 2 ms (100.00%), Space: 98.1 MB (100.00%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.java
+++ 3152-special-array-ii/3152-special-array-ii.java
@@ -2,26 +2,24 @@ class Solution {
 
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
         int n = queries.length;
-        boolean ans[] = new boolean[n];
+        
         int arr[] = new int[nums.length];
         int count = 0;
-        arr[0]=count;
+        // arr[0]=count;
         
         for(int i=1;i<nums.length;i++){
-            if(nums[i]%2==0 && nums[i-1]%2==1) arr[i]=count;
-            else if(nums[i]%2==1 && nums[i-1]%2==0) arr[i]=count;
-            else{
-                count++;
-                arr[i]=count;
-            }
+            if(nums[i]%2==0 && nums[i-1]%2==0) count++;
+            else if(nums[i]%2==1 && nums[i-1]%2==1) count++;
+            arr[i]=count;
         }
         
+        boolean ans[] = new boolean[n];
         for(int i=0;i<n;i++){
             int a = queries[i][0];
             int b = queries[i][1];
             
             if(arr[a]==arr[b]) ans[i]=true;
-            else ans[i]=false;
+            // else ans[i]=false;
         }
         
         return ans;
