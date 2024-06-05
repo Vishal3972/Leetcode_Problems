57fd41c920b272ab6af958f3b06140fd5c2f912b
Vishal3972 <125539507+Vishal3972@users.noreply.github.com>
2024-06-05 10:22:07 +0530

    Time: 6 ms (44.14%), Space: 43.1 MB (68.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,14 +4,14 @@ class Solution {
             return false;
         }
         int ch[] = new int[26];
+        
         for(int i=0; i<s.length(); i++){
             int temp = s.charAt(i)-'a';
             ch[temp] = ch[temp]+1;
-        }
-        for(int i=0; i<t.length(); i++){
-            int temp = t.charAt(i)-'a';
+            temp = t.charAt(i)-'a';
             ch[temp] = ch[temp]-1;
         }
+        
         for(int i=0; i<ch.length; i++){
             if(ch[i] != 0){
                 return false;
